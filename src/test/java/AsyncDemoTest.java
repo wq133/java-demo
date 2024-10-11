322d02c13e41fb682f4f4a857a6627414f77ad02
wq133 <wqh666@vip.qq.com>
2024-10-11 16:03:31 +0800

    1011 AsyncInitDemo

---
+++ src/test/java/AsyncDemoTest.java
@@ -0,0 +1,9 @@
+/**
+ * 参考链接：    https://www.cnblogs.com/cfas/p/16110488.html
+ *
+ * Async失效原因：https://www.jb51.net/program/312801vad.htm
+ *
+ */
+//@SpringBootTest("")
+public class AsyncDemoTest {
+}
